54f131af0e59fd4b671a1d68191ec30ce59d920a
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2022-01-18 09:51:29 +0900

    コメント修正

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -18,32 +18,29 @@ public class UserDAO {
 
 	/**
 	 * ユーザーを全件取得します
-	 * @param Connection connection データベースコネクションのインスタンス
+	 * 
+	 * @param Connection connection データベース接続のインスタンス
 	 * @return 検索結果（ユーザーモデルのリスト）
 	 */
 	public List<UserModel> findAll(Connection connection) {
 		List<UserModel> list = new ArrayList<UserModel>();
 		try {
-			// データベースに接続する
-//			DBConnection db = new DBConnection();
-//			try (Connection connection = db.getConnection()) {
-				// SQLを実行する
-				String sql = "select * from users order by id";
-				try (PreparedStatement stmt = connection.prepareStatement(sql); ResultSet rs = stmt.executeQuery()) {
-					// SQLの実行結果をArrayListに格納する
-					while (rs.next()) {
-						UserModel model = new UserModel();
-						model.setId(rs.getInt("id"));
-						model.setEmail(rs.getString("email"));
-						model.setPassword(rs.getString("todo_item"));
-						model.setName(rs.getString("name"));
-						model.setIsDeleted(rs.getInt("is_deleted"));
-						model.setCreatedAt(Timestamp.valueOf(rs.getString("created_at")));
-						model.setUpdatedAt(Timestamp.valueOf(rs.getString("updated_at")));
-						list.add(model);
-					}
+			// SQLを実行する
+			String sql = "select * from users order by id";
+			try (PreparedStatement stmt = connection.prepareStatement(sql); ResultSet rs = stmt.executeQuery()) {
+				// SQLの実行結果をArrayListに格納する
+				while (rs.next()) {
+					UserModel model = new UserModel();
+					model.setId(rs.getInt("id"));
+					model.setEmail(rs.getString("email"));
+					model.setPassword(rs.getString("todo_item"));
+					model.setName(rs.getString("name"));
+					model.setIsDeleted(rs.getInt("is_deleted"));
+					model.setCreatedAt(Timestamp.valueOf(rs.getString("created_at")));
+					model.setUpdatedAt(Timestamp.valueOf(rs.getString("updated_at")));
+					list.add(model);
 				}
-//			}
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
@@ -53,37 +50,34 @@ public class UserDAO {
 
 	/**
 	 * 指定ユーザーIDのユーザーを1件検索します
-	 * @param Connection connection データベースコネクションのインスタンス
-	 * @param id ユーザーID
+	 * 
+	 * @param Connection connection データベース接続のインスタンス
+	 * @param id         ユーザーID
 	 * @return 検索結果（ユーザーモデル）
 	 */
 	public UserModel findOne(Connection connection, int id) {
 		UserModel model = new UserModel();
 		try {
-			// データベースに接続する
-//			DBConnection db = new DBConnection();
-//			try (Connection connection = db.getConnection()) {
-				// SQLを実行する準備をする
-				String sql = "select * from users where id=?";
-				try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-					// パラメータを設定する
-					stmt.setInt(1, id);
-					// SQLを実行する
-					try (ResultSet rs = stmt.executeQuery()) {
-						if (rs.next()) {
-							model.setId(rs.getInt("id"));
-							model.setEmail(rs.getString("email"));
-							model.setPassword(rs.getString("todo_item"));
-							model.setName(rs.getString("name"));
-							model.setIsDeleted(rs.getInt("is_deleted"));
-							model.setCreatedAt(Timestamp.valueOf(rs.getString("created_at")));
-							model.setUpdatedAt(Timestamp.valueOf(rs.getString("updated_at")));
-						} else {
-							model = null;
-						}
+			// SQLを実行する準備をする
+			String sql = "select * from users where id=?";
+			try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+				// パラメータを設定する
+				stmt.setInt(1, id);
+				// SQLを実行する
+				try (ResultSet rs = stmt.executeQuery()) {
+					if (rs.next()) {
+						model.setId(rs.getInt("id"));
+						model.setEmail(rs.getString("email"));
+						model.setPassword(rs.getString("todo_item"));
+						model.setName(rs.getString("name"));
+						model.setIsDeleted(rs.getInt("is_deleted"));
+						model.setCreatedAt(Timestamp.valueOf(rs.getString("created_at")));
+						model.setUpdatedAt(Timestamp.valueOf(rs.getString("updated_at")));
+					} else {
+						model = null;
 					}
 				}
-//			}
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
@@ -93,40 +87,36 @@ public class UserDAO {
 
 	/**
 	 * 指定ユーザーIDのユーザーを1件検索します
-	 * @param Connection connection データベースコネクションのインスタンス
-	 * @param id ユーザーID
+	 * 
+	 * @param Connection connection データベース接続のインスタンス
+	 * @param id         ユーザーID
 	 * @return 検索結果（ユーザーモデル）
 	 */
 	public UserModel findOne(Connection connection, String email, String password) {
 		UserModel model = new UserModel();
 		try {
-			// データベースに接続する
-//			DBConnection db = new DBConnection();
-//			try (Connection connection = db.getConnection()) {
-				// SQLを実行する準備をする
-				String sql = "select * from users "
-						+ "where is_deleted=0 and email=? and password=?";
-				try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-					// パラメータを設定する
-					stmt.setString(1, email);
-					stmt.setString(2, password);
-					// SQLを実行する
-					try (ResultSet rs = stmt.executeQuery()) {
-						// SQLの実行結果を取得する
-						if (rs.next()) {
-							model.setId(rs.getInt("id"));
-							model.setEmail(rs.getString("email"));
-							model.setPassword(rs.getString("password"));
-							model.setName(rs.getString("name"));
-							model.setIsDeleted(rs.getInt("is_deleted"));
-							model.setCreatedAt(Timestamp.valueOf(rs.getString("created_at")));
-							model.setUpdatedAt(Timestamp.valueOf(rs.getString("updated_at")));
-						} else {
-							model = null;
-						}
+			// SQLを実行する準備をする
+			String sql = "select * from users " + "where is_deleted=0 and email=? and password=?";
+			try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+				// パラメータを設定する
+				stmt.setString(1, email);
+				stmt.setString(2, password);
+				// SQLを実行する
+				try (ResultSet rs = stmt.executeQuery()) {
+					// SQLの実行結果を取得する
+					if (rs.next()) {
+						model.setId(rs.getInt("id"));
+						model.setEmail(rs.getString("email"));
+						model.setPassword(rs.getString("password"));
+						model.setName(rs.getString("name"));
+						model.setIsDeleted(rs.getInt("is_deleted"));
+						model.setCreatedAt(Timestamp.valueOf(rs.getString("created_at")));
+						model.setUpdatedAt(Timestamp.valueOf(rs.getString("updated_at")));
+					} else {
+						model = null;
 					}
 				}
-//			}
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
@@ -136,31 +126,24 @@ public class UserDAO {
 
 	/**
 	 * ユーザーを1件追加します
-	 * @param Connection connection データベースコネクションのインスタンス
-	 * @param model UserModel
+	 * 
+	 * @param Connection connection データベース接続のインスタンス
+	 * @param model      UserModel
 	 * @return 実行結果 1:成功、0:失敗、その他:エラーコード
 	 */
 	public int create(Connection connection, UserModel model) {
 		try {
-			// データベースに接続する
-//			DBConnection db = new DBConnection();
-//			try (Connection connection = db.getConnection()) {
-				// SQLを実行する準備をする
-				String sql = "insert into users (email, password, name, is_deleted) "
-						+ "values (?, ?, ?, ?)";
-				try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-					// パラメータを設定する
-					stmt.setString(1, model.getEmail());
-					stmt.setString(2, model.getPassword());
-					stmt.setString(3, model.getName());
-					stmt.setInt(4, model.getIsDeleted());
-					// SQLを実行する
-					stmt.executeUpdate();
-				}
-//			}
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//			return DatabaseSettings.DB_EXECUTION_FAILURE;
+			// SQLを実行する準備をする
+			String sql = "insert into users (email, password, name, is_deleted) " + "values (?, ?, ?, ?)";
+			try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+				// パラメータを設定する
+				stmt.setString(1, model.getEmail());
+				stmt.setString(2, model.getPassword());
+				stmt.setString(3, model.getName());
+				stmt.setInt(4, model.getIsDeleted());
+				// SQLを実行する
+				stmt.executeUpdate();
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return e.getErrorCode();
@@ -170,32 +153,27 @@ public class UserDAO {
 
 	/**
 	 * 指定ユーザーIDのユーザーを1件更新します。
+	 * 
+	 * @param Connection connection データベース接続のインスタンス
 	 * @param model
 	 * @return 実行結果 1:成功、0:失敗、その他:エラーコード
 	 */
 	public int update(Connection connection, UserModel model) {
 		try {
-			// データベースに接続する
-//			DBConnection db = new DBConnection();
-//			try (Connection connection = db.getConnection()) {
-				// SQLを実行する準備をする
-				String sql = "update users set "
-						+ "email=?, password=?, name=?, is_deleted=? "
-						+ "where id=?";
-				try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-					// パラメータを設定する
-					stmt.setString(1, model.getEmail());
-					stmt.setString(2, model.getPassword());
-					stmt.setString(3, model.getName());
-					stmt.setInt(4, model.getIsDeleted());
-					stmt.setInt(5, model.getId());
-					// SQLを実行する
-					stmt.executeUpdate();
-				}
-//			}
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//			return DatabaseSettings.DB_EXECUTION_FAILURE;
+			// SQLを実行する準備をする
+			String sql = "update users set " 
+					+ "email=?, password=?, name=?, is_deleted=? " 
+					+ "where id=?";
+			try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+				// パラメータを設定する
+				stmt.setString(1, model.getEmail());
+				stmt.setString(2, model.getPassword());
+				stmt.setString(3, model.getName());
+				stmt.setInt(4, model.getIsDeleted());
+				stmt.setInt(5, model.getId());
+				// SQLを実行する
+				stmt.executeUpdate();
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return e.getErrorCode();
